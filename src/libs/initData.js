8e8f7f9bf4a5aee57a06162ea1e245bfd33f5fae
Goolory <15083500804@139.com>
2018-04-15 15:06:58 +0800

    test

--- src/libs/initData.js
+++ src/libs/initData.js
@@ -1,8 +1,8 @@
 export let studentMenu = [
     { path: '/page1', name: '实验列表', iconCls: 'el-icon-message' },
-    { path: '/page2', name: 'page2', iconCls: 'el-icon-message' },
-    { path: '/page2', name: 'page2', iconCls: 'el-icon-message' },
-    { path: '/page2', name: 'page2', iconCls: 'el-icon-message' },
+    { path: '/page2', name: '课程作业', iconCls: 'el-icon-message' },
+    { path: '/page2', name: '在线测试', iconCls: 'el-icon-message' },
+    { path: '/page2', name: '答疑讨论', iconCls: 'el-icon-message' },
     { path: '/page2', name: 'page2', iconCls: 'el-icon-message' },
     { path: '/page2', name: 'page2', iconCls: 'el-icon-message' },
 ]
